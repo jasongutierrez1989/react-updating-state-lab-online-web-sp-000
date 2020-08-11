024a9babf48d2d4371fe8c5c02125ea31695bd16
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-08-11 23:47:15 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,7 +1,20 @@
 import React, { Component }from 'react'
 
-export default class DigitalClicker extends Component {
+class DigitalClicker extends Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  handleClick = () => {
+    this.setState({
+      timesClicked: this.state.timesClicked + 1
+    })
+  }
+  
   render(){
 
   }
